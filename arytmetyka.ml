bdbc90b9cac4b1f15675d12dae9b454eb65887bf
Artur Matyjasek <am406242@students.mimuw.edu.pl>
2018-10-22 22:36:04 +0200

    poprawiony typ wartosc

--- arytmetyka.ml
+++ arytmetyka.ml
@@ -1,32 +1,47 @@
 
-type wartosc = Wartosc of float * float * wartosc
+type przedzial = Przedzial of float * float | Pelny | Pusty
+
+type wartosc = przedzial list
   
 let wartosc_dokladnosc x p =
   let delta = abs_float(p /. 100. *. x) in
-  Wartosc(x -. delta , x +. delta)
+  (Przedzial(x -. delta , x +. delta)::[]:wartosc)
 
-let wartosc_od_do x y = Wartosc(x,y)
+let wartosc_od_do x y = (Przedzial(x,y)::[]:wartosc)
 
-let wartosc_dokladna x = Wartosc(x,x)
+let wartosc_dokladna x = (Przedzial(x,x)::[]:wartosc)
 
 let in_wartosc (w:wartosc) (a:float) =
-  let Wartosc(x, y) = w in
-  if x >= a && y <= a then true
-  else false
+  let rec pom (w:wartosc) (a:float) =
+    match w with
+    | [] -> false
+    | h::t -> let Przedzial(x, y) = h in
+      if x <= a && y >= a then true
+      else pom t a
     
 let min_wartosc w =
-  let Wartosc(x, y) = w in
-  x
+  let rec pom w mini =
+    match w with
+    | [] -> mini
+    | h::t -> let Przedzial(x, y) = h in
+      pom t (min mini x)
 
 let max_wartosc w =
-  let Wartosc(x, y) = w in
-  y
+  let rec pom w maks =
+    match w with
+    | [] -> maks
+    | h::t -> let Przedzial(x, y) = h in
+      pom t (max maks x)
 
 let sr_wartosc w =
-  let Wartosc(x, y) = w in
-  if x = neg_infinity || y = infinity then nan
-  else (x +. y) /. 2.
+  match w with
+  | h::[] -> let Przedzial(x, y) = h in
+    if x = neg_infinity || y = infinity then nan
+    else (x +. y) /. 2.
+  | h::t -> nan
+    
  
 let () = assert(1 <= 2)
+
 
 
